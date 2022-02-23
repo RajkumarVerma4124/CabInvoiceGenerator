7d26d4a5ecfb201c9a7f62348bfcda7b3b13d882
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-23 10:58:21 +0530

    [Raj] Added:- The code to calculate normal fare for cab ride

---
+++ CabInvoiceGeneratorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CabInvoiceGeneratorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CabInvoiceGeneratorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b3784e74-1456-4371-82b9-a63b6197813d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
