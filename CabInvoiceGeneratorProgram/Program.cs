0dda3b0275f91bac0d222c8d0a980ca3a96aa1ad
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-23 09:05:14 +0530

    [Raj] Added:- The welcome message in the main program

---
+++ CabInvoiceGeneratorProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGeneratorProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Displaying welcome message
+            Console.WriteLine("Welcome To The Cab Invoice Generator Program");
+            Console.ReadLine();
+        }
+    }
+}
